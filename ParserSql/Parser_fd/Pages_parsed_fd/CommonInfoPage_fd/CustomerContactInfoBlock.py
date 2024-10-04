ef25e977396dbf1fac7c4484d79189784917b73f
AlexServGE <alexander.sergeevru@yandex.ru>
2024-10-04 10:06:02 +0300

    JavaSpring added jar file

--- ParserSql/Parser_fd/Pages_parsed_fd/CommonInfoPage_fd/CustomerContactInfoBlock.py
+++ ParserSql/Parser_fd/Pages_parsed_fd/CommonInfoPage_fd/CustomerContactInfoBlock.py
@@ -29,14 +29,3 @@ if __name__ == '__main__':
         'div', 'col')[1].get_text(strip=True, separator="|").split("|")
     idx_key_region = customer_contact_info_block.index("Место нахождения")
     print(customer_contact_info_block[idx_key_region + 1])
-
-
-"""  File "D:\IT_scholarship\My_python_apps\Diplom_Pharmabot\Parser_Bd\Parser_fd\Pages_parsed_fd\CommonInfoPage.py", line 14, in __init__
-    self.CustomerContactInfoBlock = CustomerContactInfoBlock(soup)
-  File "D:\IT_scholarship\My_python_apps\Diplom_Pharmabot\Parser_Bd\Parser_fd\Pages_parsed_fd\CommonInfoPage_fd\CustomerContactInfoBlock.py", line 5, in __init__
-    self.procurement_customer_address = self.get_address(customer_contact_info_block)  # Российская Федерация, 663491, Красноярский край, Кежемский р-н, Кодинск г, Гидростроителей, Д. 26
-  File "D:\IT_scholarship\My_python_apps\Diplom_Pharmabot\Parser_Bd\Parser_fd\Pages_parsed_fd\CommonInfoPage_fd\CustomerContactInfoBlock.py", line 13, in get_address
-    idx_key_address = customer_contact_info_block.index("Место нахождения")
-  File "D:\IT_scholarship\My_python_apps\Diplom_Pharmabot\Parser_Bd\venv\lib\site-packages\bs4\element.py", line 1541, in index
-    raise ValueError("Tag.index: element not in tag")
-ValueError: Tag.index: element not in tag"""
